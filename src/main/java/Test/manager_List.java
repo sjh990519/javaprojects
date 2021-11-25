358954673267c0620392bccb533069c5bb78ef49
sjh990519 <sjh990519@gmail.com>
2021-11-25 16:45:37 +0900

    음식판 합친 version

--- src/main/java/Test/manager_List.java
+++ src/main/java/Test/manager_List.java
@@ -163,7 +163,11 @@ public class manager_List extends javax.swing.JFrame {
     
     // All_Food Management (식당 전체 관리)
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
+
+        Menu_list menu = new Menu_list();
+        menu.setVisible(true);
+        setVisible(false);
+        
     }//GEN-LAST:event_jButton3ActionPerformed
 
     
